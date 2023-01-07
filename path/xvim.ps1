2809f9b0d0d11b72c1e2875a0cf987d14f51b5c1
Everton Jr <69195288+evertonse@users.noreply.github.com>
2023-01-07 10:40:16 -0800

    Add files via upload

---
+++ path/xvim.ps1
@@ -0,0 +1,16 @@
+#Requires -Version 7.1
+$ErrorActionPreference = "Stop" # exit when command fails
+
+$env:XVIM_BASE_PATH   = "C:\Users\ExCyber\AppData\Local\nvim-astrovim"
+
+$env:XDG_DATA_HOME    = "$env:XVIM_BASE_PATH"   
+$env:XDG_CONFIG_HOME  = "$env:XVIM_BASE_PATH"   
+$env:XDG_CACHE_HOME   = "$env:XVIM_BASE_PATH"   
+
+#$env:XDG_RUNTIME_DIR  = "$env:XDG_DATA_HOME\nvim.astrovim"
+# $env:XDG_CONFIG_DIR   = "$env:XDG_CONFIG_HOME\nvim.astrovim"
+# $env:XDG_CACHE_DIR    = "$env:XDG_CACHE_HOME\nvim.astrovim-data"
+# $env:XDG_BASE_DIR     = "$env:LUNARVIM_RUNTIME_DIR\nvim.astrovim"
+
+nvim  -u "$env:XVIM_BASE_PATH\nvim\init.lua" @args
+
