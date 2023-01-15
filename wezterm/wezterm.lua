cbdb4b1c52c666a2370589226fe8489441d0eb14
Everton Jr <69195288+evertonse@users.noreply.github.com>
2023-01-15 04:26:57 -0300

    Add files via upload

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,11 +17,14 @@ local wezterm = require 'wezterm'
 return {
   -- Run any program with default args
   --default_prog = {'C:\\Windows\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe'},
-  --default_prog = {[[C:\\Program Files\\PowerShell\\7\\pwsh.exe]], '-NoLogo'}, -- Powershell 7.+
-  default_prog = {[[C:\\Program Files\\PowerShell\\7\\pwsh.exe]], 
-    '-NoLogo','-noe','-c',
-   [[&{Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell ccb1610a}]] },
+  default_prog = {[[C:\\Program Files\\PowerShell\\7\\pwsh.exe]], '-NoLogo'}, -- Powershell 7.+
 
+
+  -- default_prog = {[[C:\\Program Files\\PowerShell\\7\\pwsh.exe]], 
+  --   '-NoLogo','-noe','-c',
+  --  [[&{Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell ccb1610a}]] },
+  --
+  --
 
   window_background_opacity = 0.92,
   font = wezterm.font 'MesloLGS NF',
@@ -48,6 +51,22 @@ return {
     border_right_width    = '0px',
     border_bottom_height  = '0px',
     border_top_height     = '0px',
+  },
+
+  --wezterm.action.CloseCurrentPane { confirm = false },
+  --wezterm.action.CloseCurrentTab { confirm = false },
+
+
+  skip_close_confirmation = true,
+  skip_close_confirmation_for_processes_named = {
+    'cmd.exe',
+    'nvim.exe',
+    'pwsh.exe',
+    'powershell.exe',
+    'sh',
+    'zsh',
+    'fish',
+    'tmux',
   },
   colors = {
     -- The default text color
