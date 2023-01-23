43c26c4ecfa6b481ad09281f491fbf3174c807fd
Everton Jr <evertonse.junior@gmail.com>
2023-01-22 21:09:38 -0300

    better alacritty setting for when using branch with openconsole suport

--- nvim/after/plugin/telescope.lua
+++
@@ -1,5 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader><leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader><leader>s', function()
-	builtin.grep_string({search = vim.fn.input("grep :")})
-end)
