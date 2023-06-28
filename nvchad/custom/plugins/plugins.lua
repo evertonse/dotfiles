ed51f25f55d00c22e8f3006fa78676ea01e83bbb
Everton ExCyber <evertonse.junior@gmail.com>
2023-06-28 04:00:57 -0300

    nvchad

--- nvchad/custom/plugins/plugins.lua
+++ nvchad/custom/plugins/plugins.lua
@@ -102,7 +102,38 @@ M.plugins = {
     --{'evertonse/nvim-treesitter'},
     {"JoosepAlviste/nvim-ts-context-commentstring",     }, -- Nice Vim commenting --  context_commentstring { enable = true },
     -- {'David-Kunz/markid',                                                                       }, -- Every identifier has the same color
-    {'nvim-treesitter/playground', enable=true                      },
+    {
+      'nvim-treesitter/playground', 
+      lazy=false,
+      config = function() 
+        require "nvim-treesitter.configs".setup {
+          playground = {
+            enable = true,
+            disable = {},
+            updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+            persist_queries = false, -- Whether the query persists across vim sessions
+            keybindings = {
+              toggle_query_editor = 'o',
+              toggle_hl_groups = 'i',
+              toggle_injected_languages = 't',
+              toggle_anonymous_nodes = 'a',
+              toggle_language_display = 'I',
+              focus_language = 'f',
+              unfocus_language = 'F',
+              update = 'R',
+              goto_node = '<cr>',
+              show_help = '?',
+            },
+          },
+          query_linter = {
+            enable = true,
+            use_virtual_text = true,
+            lint_events = {"BufWrite", "CursorHold"},
+          },
+      }
+      end, 
+      enable=true                      
+    },
 
     -- Argument Coloring
     -- {'octol/vim-cpp-enhanced-highlight',                }, -- still haven't d but adds cpp keywords for highlight tweak even further
