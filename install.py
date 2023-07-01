fbc354ac4bb285a0c487e5137dbbc127b8e166e3
Everton Jr <evertonse.junior@gmail.com>
2023-06-30 23:10:18 -0300

    installing dotfiles support

--- install.py
+++ install.py
@@ -1,17 +1,38 @@
+#!/bin/python3
+import sys
+import os
 from pathlib import Path
 import vscode.install as vs
+from shutil import copytree as copydir, rmtree as rmdir, copy as cp
 from glob import glob 
 
+home = Path.home()
 installs = {
-    1: "vscode"
+    1: "vscode",
+    2: "dotfiles",
 }
 
-print("Which one do you wish to install ? ")
-for key,value in installs.items():
-    print(f'{key} : {value}')
+def dotfiles():
+    files = ['./.bashrc', './.tmux.conf']
+    for f in files:
+        f = Path(f)
+        if input(f'About to copy and overwrite src={f} into dest={Path(home,f)} [y/n]?').lower() == 'y':
+            cp(f, Path(home,f))
 
-which = installs[int(input())]
-if which == "vscode":
-    vs.install()
+def main():
+    if os.name == 'nt':
+        print('Windows not supported for automatically install')
+        exit(69)
 
+    print("Which one do you wish to install ? ")
+    for key,value in installs.items():
+        print(f'{key} : {value}')
 
+    which = installs[int(input())]
+    if which == "vscode":
+        vs.install()
+
+    elif which == 'dotfiles':
+        dotfiles()
+
+main()
