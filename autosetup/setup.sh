621c255e38244b5b63af03c110cf586aed3c7f0e
evertonse <evertonse.junior@gmail.com>
2023-07-13 16:43:21 -0300

    balling just fine

--- autosetup/setup.sh
+++ autosetup/setup.sh
@@ -2,11 +2,26 @@
 sudo pacman -Syu --noconfirm git make cmake
 
 # @firmware
-sudo pacman -Syu --noconfirm fwupd
-sudo fwupdmgr get-devices
-sudo fwupdmgr refresh --force
-sudo fwupdmgr get-updates -y
-sudo fwupdmgr update -y
+read -p "Do you want install firmware and update it with fwupd? [y/n]: " answer
+
+if [ "$answer" = "y" ]; then
+  sudo pacman -Syu --noconfirm fwupd
+  sudo fwupdmgr get-devices
+  sudo fwupdmgr refresh --force
+  sudo fwupdmgr get-updates -y
+  sudo fwupdmgr update -y
+else
+    echo "No action needed was done about firmware ."
+fi
+
+# linux idk you'll need
+
+read -p "Do you want install linux-header, base-devel and linux firmware stuff ? [y/n]: " answer
+if [ "$answer" = "y" ]; then
+  sudo pacman -S base-devel linux linux-headers linux-api-headers linux-firmware linux-docs --noconfirm
+else
+  echo "No action needed was done about linux stuff."
+fi
 
 # @radare
 git clone https://github.com/radare/radare2 ~/code/radare && cd ~/code/radare && sh sys/install.sh
@@ -15,8 +30,6 @@ sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/ya
 # @neovim
 git clone https://github.com/neovim/neovim.git ~/code/neovim && cd ~/code/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
 
-# linux idk you'll need
-sudo pacman -S base-devel linux linux-headers linux-api-headers linux-firmware linux-docs --noconfirm
 # @x11 stuff
 sudo pacman -S --noconfirm xcb-proto xcb-util xcb-util-wm xcb-util-cursor xcb-util-keysyms
 sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xsetroot
