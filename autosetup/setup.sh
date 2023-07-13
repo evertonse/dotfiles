493f47580340ecc55b5443eb808d9d52eb012aa0
evertonse <evertonse.junior@gmail.com>
2023-07-13 17:06:51 -0300

    setup

--- autosetup/setup.sh
+++ autosetup/setup.sh
@@ -1,6 +1,7 @@
 # @basic
+echo " This script assumes every git clone will be placed under ~/code folder"
+echo " if this folder already has a certain repo, no action will be taken"
 sudo pacman -Syu --noconfirm git make cmake
-
 # @firmware
 read -p "Do you want install firmware and update it with fwupd? [y/n]: " answer
 
@@ -24,26 +25,42 @@ else
 fi
 
 # @radare
-git clone https://github.com/radare/radare2 ~/code/radare && cd ~/code/radare && sh sys/install.sh
+
+read -p "Do you want install radare ? [y/n]: " answer
+if [ "$answer" = "y" ]; then
+  git clone https://github.com/radare/radare2 ~/code/radare 
+  cd ~/code/radare 
+  sh sys/install.sh
+fi
+
 # @yay
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git ~/code/yay && cd ~/code/yay && makepkg -si
+read -p "Do you want install yay? [y/n]: " answer
+if [ "$answer" = "y" ]; then
+  sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git ~/code/yay && cd ~/code/yay && makepkg -si
+fi
+
 # @neovim
-git clone https://github.com/neovim/neovim.git ~/code/neovim && cd ~/code/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
+read -p "Do you want install neovim? [y/n]: " answer
+if [ "$answer" = "y" ]; then
+  git clone https://github.com/neovim/neovim.git ~/code/neovim && cd ~/code/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
+fi
 
 # @x11 stuff
-sudo pacman -S --noconfirm xcb-proto xcb-util xcb-util-wm xcb-util-cursor xcb-util-keysyms
-sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xsetroot
+
+read -p "Do you want install X11 stuff? [y/n]: " answer
+if [ "$answer" = "y" ]; then
+  sudo pacman -S --noconfirm xcb-proto xcb-util xcb-util-wm xcb-util-cursor xcb-util-keysyms
+  sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xsetroot
+fi
+
+read -p "Do you want install suckless and rocks stuff? [y/n]: " answer
+if [ "$answer" = "y" ]; then
+  git clone https://github.com/evertonse/rocks.git ~/code/rocks && cd ~/code/rocks && ./install.sh
+fi
 
 # @user pkg
-sudo pacman -S --noconfirm zathura
-sudo pacman -S glow --noconfirm
-sudo pacman -S lf --noconfirm
-sudo pacman -S mpv --noconfirm
-sudo pacman -S zsh --noconfirm
+sudo pacman -S --noconfirm zathura glow lf mpv zsh tmux
 #sudo pacman -S neovim --noconfirm
 # python-pywal -> wal
 sudo pacman -S xwallpaper picom python-pywal ueberzug --noconfirm
-sudo pacman -S tmux --noconfirm
-yay --noconfirm vivid fd ripgrep
-
-
+yay --noconfirm vivid fd ripgrep wget
