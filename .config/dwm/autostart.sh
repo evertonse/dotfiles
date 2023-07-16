8ed9caa987797c68cd3b8fbf69fb6e4ddf1ede2d
evertonse <evertonse.junior@gmail.com>
2023-07-16 11:16:24 -0300

    better remap keys

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -2,7 +2,11 @@
 xset r rate 186 20
 xset s off
 xset s off -dpms
-picom --config ~/.config/picom/picon.conf & 
+xset s off dpms
+xmodmap -e "clear lock"
+xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
+xmodmap -e "keycode 66 = Escape NoSymbol Escape"
+#picom --config ~/.config/picom/picon.conf & # enable it if your pc is a little faster
 xwallpaper --zoom ~/Pictures/681587.png &
 exec dwmblocks &
 #exec slstatus &
