7e35a952bebccd1ac306ce1b3a4a39de9c528409
evertonse <evertonse.junior@gmail.com>
2023-07-16 11:20:04 -0300

    pkill picom

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -7,6 +7,7 @@ xmodmap -e "clear lock"
 xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
 xmodmap -e "keycode 66 = Escape NoSymbol Escape"
 #picom --config ~/.config/picom/picon.conf & # enable it if your pc is a little faster
+pkill picom
 xwallpaper --zoom ~/Pictures/681587.png &
 exec dwmblocks &
 #exec slstatus &
