080fdc08080d361feca08c7d3cbed8b4385895fe
evertonse <evertonse.junior@gmail.com>
2023-08-06 01:52:54 -0300

    Sun Aug  6 01:52:54 AM -03 2023

--- .local/include/excyber/common.h
+++ .local/include/excyber/common.h
@@ -155,6 +155,65 @@ typedef int8_t   i8;
 		#define null  ((void *)0)
 	#endif
 #endif
+  // __linux__       Defined on Linux
+  //   __sun           Defined on Solaris
+  //   __FreeBSD__     Defined on FreeBSD
+  //   __NetBSD__      Defined on NetBSD
+  //   __OpenBSD__     Defined on OpenBSD
+  //   __APPLE__       Defined on Mac OS X
+  //   __hpux          Defined on HP-UX
+  //   __osf__         Defined on Tru64 UNIX (formerly DEC OSF1)
+  //   __sgi           Defined on Irix
+  //   _AIX            Defined on AIX
+  //   _WIN32          Defined on Windows
+
+extern "C" {
+inline const char*
+arch_build() { //Get current architecture, detectx nearly every architecture. Coded by Freak
+  #if defined(__x86_64__) || defined(_M_X64)
+  return "x86_64";
+  #elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+  return "x86_32";
+  #elif defined(__ARM_ARCH_2__)
+  return "ARM2";
+  #elif defined(__ARM_ARCH_3__) || defined(__ARM_ARCH_3M__)
+  return "ARM3";
+  #elif defined(__ARM_ARCH_4T__) || defined(__TARGET_ARM_4T)
+  return "ARM4T";
+  #elif defined(__ARM_ARCH_5_) || defined(__ARM_ARCH_5E_)
+  return "ARM5"
+  #elif defined(__ARM_ARCH_6T2_) || defined(__ARM_ARCH_6T2_)
+  return "ARM6T2";
+  #elif defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__)
+  return "ARM6";
+  #elif defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+  return "ARM7";
+  #elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+  return "ARM7A";
+  #elif defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+  return "ARM7R";
+  #elif defined(__ARM_ARCH_7M__)
+  return "ARM7M";
+  #elif defined(__ARM_ARCH_7S__)
+  return "ARM7S";
+  #elif defined(__aarch64__) || defined(_M_ARM64)
+  return "ARM64";
+  #elif defined(mips) || defined(__mips__) || defined(__mips)
+  return "MIPS";
+  #elif defined(__sh__)
+  return "SUPERH";
+  #elif defined(__powerpc) || defined(__powerpc__) || defined(__powerpc64__) || defined(__POWERPC__) || defined(__ppc__) || defined(__PPC__) || defined(_ARCH_PPC)
+  return "POWERPC";
+  #elif defined(__PPC64__) || defined(__ppc64__) || defined(_ARCH_PPC64)
+  return "POWERPC64";
+  #elif defined(__sparc__) || defined(__sparc)
+  return "SPARC";
+  #elif defined(__m68k__)
+  return "M68K";
+  #else
+  return "UNKNOWN";
+  #endif
+} }
 
 #ifndef U8_MIN
 #define U8_MIN 0u
@@ -176,20 +235,21 @@ typedef int8_t   i8;
 #define U64_MAX 0xffffffffffffffffull
 #define I64_MIN (-0x7fffffffffffffffll - 1)
 #define I64_MAX 0x7fffffffffffffffll
-#if defined(ARCH_32_BIT)
+
+#if defined(__i386__) || UINTPTR_MAX == 0xffFFffFF
 	#define USIZE_MIX U32_MIN
 	#define USIZE_MAX U32_MAX
 
 	#define ISIZE_MIX I32_MIN
 	#define ISIZE_MAX I32_MAX
-#elif defined(ARCH_64_BIT)
+#elif defined(__amd64__) || defined(__X86_64__) || UINTPTR_MAX == 0xffFFffFFffFFffFF
 	#define USIZE_MIX U64_MIN
 	#define USIZE_MAX U64_MAX
 
 	#define ISIZE_MIX I64_MIN
 	#define ISIZE_MAX I64_MAX
 #else
-	#warning Unknown architecture size. This library only supports 32 bit and 64 bit architectures.
+	#warning Unknown platform - does not look either like 32-bit or 64-bit
 #endif
 
 #if !defined(M_PI)
