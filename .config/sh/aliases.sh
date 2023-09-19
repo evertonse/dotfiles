0fcc1e9e8e17c28936fe90549bf8d32c55fe140f
evertonse <evertonse.junior@gmail.com>
2023-09-18 23:40:35 -0300

    Mon Sep 18 11:40:35 PM -03 2023

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -25,7 +25,7 @@ alias YT='youtube-viewer'
 [ -f "$MBSYNCRC" ] && alias mbsync="mbsync -c $MBSYNCRC"
 
 #: sudo not required for some system commands
-alias shutdown=killall chrome --wait;sudo shutdown;
+alias shutdown="killall chrome --wait;sudo shutdown --now;"
 for command in backlight mount umount sv pacman updatedb su poweroff reboot ; do
 	alias $command="sudo $command"
 done; unset command
