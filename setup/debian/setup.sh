ca6dd78271e4adc4657e62582163a34fc543dd65
evertonse <evertonse.junior@gmail.com>
2023-12-09 03:44:02 -0300

    Sat Dec  9 03:44:02 AM -03 2023

--- setup/debian/setup.sh
+++ setup/debian/setup.sh
@@ -7,7 +7,6 @@ installpkg() {
 	sudo nala install -y "$1"; 
 }
 
-
 nonfree=(
   "deb http://deb.debian.org/debian/ buster main contrib non-free"
   "deb-src http://deb.debian.org/debian/ buster main contrib non-free"
@@ -33,7 +32,7 @@ read -p "nvidia ? [y/n]: " answer
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
   sudo apt install -y linux-headers-$(uname -r) build-essential gcc-multilib dkms
   sudo apt install -y nvidia-driver nvidia-kernel-dkms
-  installpkg nvidia-detect
+  source ./setup/debian/installs/nvidia.sh
 fi
 
 
@@ -49,98 +48,99 @@ wget -P /tmp/google.deb https://code.visualstudio.com/sha/download?build=stable&
 
 
 dependencies=(
-    meson
-    wget
-    ninja-build gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr libtomlplusplus3
-    make cmake python3 curl ca-certificates git ntp zsh unzip npm ripgrep
-    fd-find fzf libxft go usbutils ddcutil
-    build-essential
-    cmake
-    cmake-extras
-    curl
-    gettext
-    gir1.2-graphene-1.0
-    git
-    glslang-tools
-    gobject-introspection
-    golang
-    hwdata
-    jq
-    libavcodec-dev
-    libavformat-dev
-    libavutil-dev
-    libcairo2-dev
-    libdeflate-dev
-    libdisplay-info-dev
-    libdrm-dev
-    libegl1-mesa-dev
-    libgbm-dev
-    libgdk-pixbuf-2.0-dev
-    libgdk-pixbuf2.0-bin
-    libgirepository1.0-dev
-    libgl1-mesa-dev
-    libgraphene-1.0-0
-    libgraphene-1.0-dev
-    libgtk-3-dev
-    libgulkan-0.15-0
-    libgulkan-dev
-    libinih-dev
-    libinput-dev
-    libjbig-dev
-    libjpeg-dev
-    libjpeg62-turbo-dev
-    liblerc-dev
-    libliftoff-dev
-    liblzma-dev
-    libnotify-bin
-    libpam0g-dev
-    libpango1.0-dev
-    libpipewire-0.3-dev
-    libqt6svg6
-    libseat-dev
-    libstartup-notification0-dev
-    libswresample-dev
-    libsystemd-dev
-    libtiff-dev
-    libtiffxx6
-    libudev-dev
-    libvkfft-dev
-    libvulkan-dev
-    libvulkan-volk-dev
-    libwayland-dev
-    libwebp-dev
-    libxcb-composite0-dev
-    libxcb-cursor-dev
-    libxcb-dri3-dev
-    libxcb-ewmh-dev
-    libxcb-icccm4-dev
-    libxcb-present-dev
-    libxcb-render-util0-dev
-    libxcb-res0-dev
-    libxcb-util-dev
-    libxcb-xinerama0-dev
-    libxcb-xinput-dev
-    libxcb-xkb-dev
-    libxkbcommon-dev
-    libxkbcommon-x11-dev
-    libxkbregistry-dev
-    libxml2-dev
-    libxxhash-dev
-    openssl
-    psmisc
-    python3-mako
-    python3-markdown
-    python3-markupsafe
-    python3-yaml
-    qt6-base-dev
-    scdoc
-    seatd
-    spirv-tools
-    vulkan-validationlayers
-    vulkan-validationlayers-dev
-    wayland-protocols
-    xdg-desktop-portal
-    xwayland
+  pulsemixer
+  meson
+  wget
+  ninja-build gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr libtomlplusplus3
+  make cmake python3 curl ca-certificates git ntp zsh unzip npm ripgrep
+  fd-find fzf libxft go usbutils ddcutil
+  build-essential
+  cmake
+  cmake-extras
+  curl
+  gettext
+  gir1.2-graphene-1.0
+  git
+  glslang-tools
+  gobject-introspection
+  golang
+  hwdata
+  jq
+  libavcodec-dev
+  libavformat-dev
+  libavutil-dev
+  libcairo2-dev
+  libdeflate-dev
+  libdisplay-info-dev
+  libdrm-dev
+  libegl1-mesa-dev
+  libgbm-dev
+  libgdk-pixbuf-2.0-dev
+  libgdk-pixbuf2.0-bin
+  libgirepository1.0-dev
+  libgl1-mesa-dev
+  libgraphene-1.0-0
+  libgraphene-1.0-dev
+  libgtk-3-dev
+  libgulkan-0.15-0
+  libgulkan-dev
+  libinih-dev
+  libinput-dev
+  libjbig-dev
+  libjpeg-dev
+  libjpeg62-turbo-dev
+  liblerc-dev
+  libliftoff-dev
+  liblzma-dev
+  libnotify-bin
+  libpam0g-dev
+  libpango1.0-dev
+  libpipewire-0.3-dev
+  libqt6svg6
+  libseat-dev
+  libstartup-notification0-dev
+  libswresample-dev
+  libsystemd-dev
+  libtiff-dev
+  libtiffxx6
+  libudev-dev
+  libvkfft-dev
+  libvulkan-dev
+  libvulkan-volk-dev
+  libwayland-dev
+  libwebp-dev
+  libxcb-composite0-dev
+  libxcb-cursor-dev
+  libxcb-dri3-dev
+  libxcb-ewmh-dev
+  libxcb-icccm4-dev
+  libxcb-present-dev
+  libxcb-render-util0-dev
+  libxcb-res0-dev
+  libxcb-util-dev
+  libxcb-xinerama0-dev
+  libxcb-xinput-dev
+  libxcb-xkb-dev
+  libxkbcommon-dev
+  libxkbcommon-x11-dev
+  libxkbregistry-dev
+  libxml2-dev
+  libxxhash-dev
+  openssl
+  psmisc
+  python3-mako
+  python3-markdown
+  python3-markupsafe
+  python3-yaml
+  qt6-base-dev
+  scdoc
+  seatd
+  spirv-tools
+  vulkan-validationlayers
+  vulkan-validationlayers-dev
+  wayland-protocols
+  xdg-desktop-portal
+  xwayland
 )
 
 
