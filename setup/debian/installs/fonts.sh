0c9239ee20a4ede159e03bf7f7b41e6e16d9b2c3
evertonse <evertonse.junior@gmail.com>
2023-12-09 09:07:41 -0300

    Sat Dec  9 09:07:41 AM -03 2023

--- setup/debian/installs/fonts.sh
+++ setup/debian/installs/fonts.sh
@@ -87,4 +87,27 @@ tar -xJkf JetBrainsMono.tar.xz -C ~/.local/share/fonts/JetBrainsMonoNerd 2>&1 | 
 # Update font cache and log the output
 fc-cache -v 2>&1 | tee -a "$LOG"
 
-clear
+mkdir -p ~/.local/share/fonts
+
+cd /tmp
+fonts=(
+"CascadiaCode"
+"FiraCode" 
+"Go-Mono" 
+"Hack" 
+"Inconsolata" 
+"Iosevka" 
+"JetBrainsMono" 
+"Mononoki" 
+"RobotoMono" 
+"SourceCodePro" 
+"UbuntuMono"
+)
+
+for font in ${fonts[@]}
+do
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/$font.zip
+    unzip $font.zip -d $HOME/.local/share/fonts/$font/
+    rm $font.zip
+done
+fc-cache
