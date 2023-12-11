04db4b69577756b5b9e48cdb06ae2ae64232b23c
evertonse <evertonse.junior@gmail.com>
2023-12-10 22:12:55 -0300

    Sun Dec 10 10:12:55 PM -03 2023

--- .config/sh/startup.sh
+++ .config/sh/startup.sh
@@ -1,2 +1,17 @@
 mkdir /mnt/ntfs1
 echo "22041298" | sudo -S mount -t ntfs /dev/nvme0n1p3 /mnt/ntfs1
+
+pkill pipewire &
+systemctl disable --global pipewire
+systemctl disable --global pipewire-pulse
+systemctl disable --global pipewire.socket
+systemctl disable --global pipewire-pulse.socket
+
+systemctl enable  --global pulseaudio
+
+systemctl --global disable pipewire.socket
+systemctl --user stop pipewire.socket
+systemctl disable --user  pipewire
+systemctl disable --user  pipewire-pulse
+systemctl enable  --user  pulseaudio
+pulseaudio &
