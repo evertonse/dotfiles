24ccd2c6f2680e70067db4bcee60ae8f8e047d25
evertonse <evertonse.junior@gmail.com>
2023-12-12 05:18:07 -0300

    Tue Dec 12 05:18:07 AM -03 2023

--- config/sh/aliases.sh
+++ config/sh/aliases.sh
@@ -1,6 +1,7 @@
 alias tm='/bin/tmux -2 a  || /bin/tmux -2'
 alias fgrep='fgrep --color=auto'
 alias grep='grep -i --color=auto'
+alias gg='grep -rnH --color=auto'
 alias egrep='egrep --color=auto'
 alias pacman='pacman --color=auto'
 alias yay='yay --color=auto'
