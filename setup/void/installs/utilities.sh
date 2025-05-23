b32da8aec58a5ada56cd0721b6cb36c25a763d5f
evertonse <evertonse.junior@gmail.com>
2025-05-23 12:04:06 -0300

    Fri May 23 12:04:06 PM -03 2025

--- setup/void/installs/utilities.sh
+++ setup/void/installs/utilities.sh
@@ -1,8 +1,12 @@
 
 # gcompat forcompatibilitie with glibc programs that relies on ld-linux.*.so
-sudo xbps-install gcompat
+
+sudo xbps-install gcompat base-devel
 sudo xbps-install -S bat fzf fd ripgrep
 
 # NOTE: The recommended way is throught rustup stuff, bleh
 # sudo xbps-install cargo rust rustup
 # rustup default nightly
+
+sudo lua-devel
+
