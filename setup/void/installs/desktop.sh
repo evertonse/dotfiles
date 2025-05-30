af33e25fe56e9bd014e29e10f72980cc4af94f8d
evertonse <evertonse.junior@gmail.com>
2025-05-30 10:25:48 -0300

    Fri May 30 10:25:48 AM -03 2025

---
+++ setup/void/installs/desktop.sh
@@ -0,0 +1,1 @@
+sudo xbps-install -S xorg xfce4 pulseaudio
--- setup/void/installs/utilities.sh
+++ setup/void/installs/utilities.sh
@@ -1,9 +1,13 @@
 
 # gcompat forcompatibilitie with glibc programs that relies on ld-linux.*.so
 
-sudo xbps-install gcompat base-devel
+sudo xbps-install base-devel gcc g++ clang clangd
 sudo xbps-install -S bat fzf fd ripgrep
-sudo xbps-install -S xorg xfce4
+
+sudo xbps-install -S fzy curl wget
+sudo xbps-install -S python luarocks tmux
+sudo xbps-install -S yazi
+
 
 # NOTE: The recommended way is throught rustup stuff, bleh
 # sudo xbps-install cargo rust rustup
