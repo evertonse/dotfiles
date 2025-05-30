860a37977fa00a3e9b9f80756b1de6e9773200bb
evertonse <evertonse.junior@gmail.com>
2025-05-30 10:28:20 -0300

    Fri May 30 10:28:20 AM -03 2025

--- setup/void/installs/utilities.sh
+++ setup/void/installs/utilities.sh
@@ -1,12 +1,12 @@
 
 # gcompat forcompatibilitie with glibc programs that relies on ld-linux.*.so
 
-sudo xbps-install base-devel gcc g++ clang clangd
-sudo xbps-install -S bat fzf fd ripgrep
+sudo xbps-install -Sy base-devel gcc clang clangd
+sudo xbps-install -Sy bat fzf fd ripgrep
 
-sudo xbps-install -S fzy curl wget
-sudo xbps-install -S python luarocks tmux
-sudo xbps-install -S yazi
+sudo xbps-install -Sy fzy curl wget
+sudo xbps-install -Sy python luarocks tmux
+sudo xbps-install -Sy yazi
 
 
 # NOTE: The recommended way is throught rustup stuff, bleh
