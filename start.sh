75bff14cfde0c1bd9f62eadd59b88bbc26898ce4
evertonse <evertonse.junior@gmail.com>
2025-05-30 13:10:13 -0300

    Fri May 30 01:10:13 PM -03 2025

--- start.sh
+++ start.sh
@@ -8,8 +8,6 @@ start() {
 
 # start tmux
 
-# Supposedly not necessary as it'll trigger by itself once x11 starts
-start "${XDG_CONFIG_HOME:-$HOME/.config}/X11/xinitrc"
 
 
 # Check if DISPLAY is unset and if the current terminal is /dev/tty1
@@ -17,7 +15,11 @@ if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     # Check if WM_EXEC is set and not empty
     if [ -n "$WM_EXEC" ]; then
         # Uncomment to start you're window manager executable
-        exec "$WM_EXEC" &> /dev/null
+        # exec "$WM_EXEC" &> /dev/null
+        exec "$WM_EXEC" &
         # exec startx -- -keeptty -nolisten tcp > ~/.xorg.log 2>&1
     fi
 fi
+
+# Supposedly not necessary as it'll trigger by itself once x11 starts
+start "${XDG_CONFIG_HOME:-$HOME/.config}/X11/xinitrc"
