397ac7d1e2f10207846f78e2f5cc06a180709c27
evertonse <evertonse.junior@gmail.com>
2025-05-30 15:37:37 -0300

    Fri May 30 03:37:37 PM -03 2025

--- local/bin/dim-screen.sh
+++ local/bin/dim-screen.sh
@@ -1,6 +1,7 @@
-temperature=6500K
-dim=0.9
-backlight=50
+#!/usr/bin/env sh
+temperature=4500K
+dim=0.2
+backlight=9
 gammastep -x
 gammastep -l3:3 -L $temperature -b $dim:$dim &> /dev/null
 light -S $backlight
