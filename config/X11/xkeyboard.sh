c3412d1300cdebe8b7901582a6cb7e047845225a
evertonse <evertonse.junior@gmail.com>
2025-05-30 14:07:00 -0300

    Fri May 30 02:07:00 PM -03 2025

--- config/X11/xkeyboard.sh
+++ config/X11/xkeyboard.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+
+start() {
+    if command -v "$1" > /dev/null 2>&1; then
+        # Check if the program is already running
+        if ! pidof -sx "$1" &> /dev/null; then
+            "$@" &
+            echo "Started: $*" >> ~/xinit.log
+        else
+            echo "Already running: $*" >> ~/xinit.log
+        fi
+    else
+        echo "Executable not found: $1" >> ~/xinit.log
+    fi
+}
+
 # Remap keys
 start setxkbmap -option caps:escape
 start xmodmap -e "keycode 34 = BackSpace"
