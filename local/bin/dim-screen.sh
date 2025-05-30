c0e0b3bfdb7a5b32d0fe5c307dc233281da2b4ad
evertonse <evertonse.junior@gmail.com>
2025-05-30 15:54:46 -0300

    Fri May 30 03:54:46 PM -03 2025

--- local/bin/dim-screen.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-temperature=4500K
-dim=0.2
-backlight=9
-gammastep -x
-gammastep -l3:3 -m randr -O $temperature -b $dim:$dim &> /dev/null
-light -S $backlight
--- local/bin/light-screen.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-temperature=6500K
-dim=0.9
-backlight=50
-gammastep -x
-gammastep -l3:3 -m randr -O $temperature -b $dim:$dim &> /dev/null
-light -S $backlight
