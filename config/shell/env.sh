65137f52c1bedab3ab036abea88283d71747a8ee
evertonse <evertonse.junior@gmail.com>
2025-05-30 12:13:27 -0300

    Fri May 30 12:13:27 PM -03 2025

--- config/shell/env.sh
+++ config/shell/env.sh
@@ -6,6 +6,6 @@ safe_source () {
 }
 
 safe_source "${XDG_CONFIG_HOME:-$HOME/.config}/shell/src/utils/append.sh"
-safe_source "$HOME/startup.sh"
+safe_source "$HOME/start.sh"
 
 umask 0077
---
+++ start.sh
@@ -0,0 +1,4 @@
+# Put what you want to run at startup here, should be shell posix compliant and be located at $HOME/start.sh
+# You can use every functino and variabe defined in zprofile or profile or env.sh or whatever it's called the login script for your shell
+
+# safe_source "${XDG_CONFIG_HOME:-$HOME/.config}/X11/xinitrc"
