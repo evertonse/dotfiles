c398c2fa9ddc753632086f640b9e00fc3d79e3e4
evertonse <evertonse.junior@gmail.com>
2025-05-30 12:54:52 -0300

    Fri May 30 12:54:52 PM -03 2025

--- start.sh
+++ start.sh
@@ -1,6 +1,17 @@
 # Put what you want to run at startup here, should be shell posix compliant and be located at $HOME/start.sh
 # You can use every function and variabe defined in zprofile or profile or login.sh or whatever it's called the login script for your shell
 
+start() {
+  pidof -sx "$1" > /dev/null 2> /dev/null || "$@" &
+}
+
+
+# start tmux
+
+# Supposedly not necessary as it'll trigger by itself once x11 starts
+start "${XDG_CONFIG_HOME:-$HOME/.config}/X11/xinitrc"
+
+
 # Check if DISPLAY is unset and if the current terminal is /dev/tty1
 if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     # Check if WM_EXEC is set and not empty
@@ -10,14 +21,3 @@ if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
         # exec startx -- -keeptty -nolisten tcp > ~/.xorg.log 2>&1
     fi
 fi
-
-
-start() {
-  pidof -sx "$1" > /dev/null 2> /dev/null || "$@" &
-}
-
-# start tmux
-
-# Not necessary as it'll trigger by itself once x11 starts
-# start "${XDG_CONFIG_HOME:-$HOME/.config}/X11/xinitrc"
-
