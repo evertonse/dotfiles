b60d99642da6f195e03abba7f4d404c87a2e3fa5
evertonse <evertonse.junior@gmail.com>
2025-05-30 13:20:55 -0300

    Fri May 30 01:20:55 PM -03 2025

--- start.sh
+++ start.sh
@@ -15,7 +15,9 @@ if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     # Uncomment to start you're window manager executable
     # Or jsut uncomment startx IF you have `xinitrc` setup
     #
-    # After starting X11 THEN it runs xinitrc with DISPLAY set. That means you'll be able to call x's functions such as xmodmap xset
+    # After starting X11 THEN it runs xinitrc with DISPLAY set.
+    # That means you'll be able to call x's functions such as xmodmap xset
+    #
 
     # exec "$WM_EXEC" &> /dev/null
     exec startx -- -keeptty -nolisten tcp > ~/.xorg.log 2>&1
