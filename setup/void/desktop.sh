221883afc7993f7156b6a6796509fa3a2904c3ba
evertonse <evertonse.junior@gmail.com>
2025-05-31 16:56:52 -0300

    Sat May 31 04:56:52 PM -03 2025

--- setup/void/desktop.sh
+++ setup/void/desktop.sh
@@ -1,1 +1,6 @@
-sudo xbps-install -S xorg xfce4 pulseaudio
+sudo xbps-install -Syu xorg xfce4 pulseaudio
+
+sudo xbps-install -Syu elogind dbus dbus-elogind polkit-elogind
+sudo ln -sf /etc/sv/elogind /var/service/
+sudo ln -sf /etc/sv/dbus    /var/service/
+sudo ln -sf /etc/sv/polkitd /var/service/
